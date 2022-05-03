a534a770d325af6ee0c378be052075111d2280d5
Luiz Cipriano <77516067+luizcipriano@users.noreply.github.com>
2022-05-02 23:43:20 -0300

    Finished second lesson

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+          product: 'Lamp',
+          desc : 'Can make things get burned !! 🔥'
+        }
+    }
+})
